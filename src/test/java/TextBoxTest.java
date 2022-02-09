deaf77194cd2fea313112dc79061bee9a862718b
Kashtos90 <29061990a@gmail.com>
2022-02-10 00:52:56 +0700

    added body

---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void succesFillTest() {
+        open("/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+
+        $("#firstName").setValue("Sergei");
+        $("#lastName").setValue("Kashtuev");
+        $("#userEmail").setValue("kashtuev@gmail.com");
+        $("#пол").setValue("");
+        $("#userNumber").setValue("");
+        $("#датарождения").setValue("");
+        $("#сабджектс").setValue("");
+        $("#хобби").setValue("");
+        $("#uploadPicture").setValue("");
+        $("#currenAddress").setValue("Russia");
+        $("#селектштата").setValue("");
+        $("#селектгорода").setValue("");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Sergei"),text("Kashtuev"), text("kashtuev@gmail.com"));
+    }
+}
