6788e60231ff8d3d444c91538b77c579dc196e63
Kashtos90 <29061990a@gmail.com>
2022-02-09 22:58:05 +0700

    added gitignore

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,32 @@
+package junit;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+
+    @BeforeEach
+    void before() {
+        System.out.println("здрасьте");
+        Selenide.open("https://google.com");
+    }
+
+    @AfterEach
+    void after() {
+        System.out.println("забор покрасьте");
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void textTest() {
+        // поиск по тексту
+    }
+
+    @Test
+    void videoTest() {
+        // поиск видео
+    }
+}
